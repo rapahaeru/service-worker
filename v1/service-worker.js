456aa80845472039a7dde3c0627359cf94558330
Raphael Oliveira <login+rapahaeru@elo7.com>
2016-02-29 19:23:54 -0300

    v2 : desenvolvimento dos testes

--- v1/service-worker.js
+++ v1/service-worker.js
@@ -1,6 +1,6 @@
-console.log("service worker acionado");
+//console.log("service worker acionado");
 this.onfetch = function(event) { 
-	console.log("service worker acionado");
+	//console.log("service worker acionado");
 	var url = decodeURIComponent(event.request.url), 
 	// urlToMatch ='http://localhost/swexample/catchme.html',
 	urlToMatch ='http://localhost:8000/catchme.html', 
